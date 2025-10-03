244d07cb41d049a4d6e7767380c0fd42809e301a
Matthew Martin <matthewdeanmartin@gmail.com>
2025-10-02 22:25:25 -0400

    0.1.4 - check in temp files

---
+++ example/__about_cfg__.py
@@ -0,0 +1,25 @@
+"""Metadata for metametameta."""
+
+__all__ = [
+    "__title__",
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__description__",
+    "__long_description__",
+    "__keywords__",
+    "__license__",
+    "__classifiers__"
+]
+
+__title__ = "metametameta"
+__version__ = "attr: my_package.VERSION"
+__author__ = "Example Example"
+__author_email__ = "example@example.com"
+__description__ = "My package description"
+__long_description__ = "file: README.rst, CHANGELOG.rst, LICENSE.rst"
+__keywords__ = "one, two"
+__license__ = "BSD-3-Clause"
+__classifiers__ = """
+Framework :: Django
+Programming Language :: Python :: 3"""
---
+++ example/__about_pep621_2__.py
@@ -0,0 +1,13 @@
+"""Metadata for source_todo."""
+
+__all__ = [
+    "__title__",
+    "__version__",
+    "__description__",
+    "__requires_python__"
+]
+
+__title__ = "source_todo"
+__version__ = "0.1.0"
+__description__ = "TODOs in source code as a first class construct"
+__requires_python__ = ">=3.9"
