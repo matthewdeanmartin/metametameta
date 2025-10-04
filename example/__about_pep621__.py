1b7a4b7b50f7f4088460da3265145ba03bfd9923
Matthew Martin <matthewdeanmartin@gmail.com>
2025-10-04 15:26:15 -0400

    0.1.5: more deps

---
+++ example/__about_pep621__.py
@@ -0,0 +1,21 @@
+"""Metadata for metametameta."""
+
+__all__ = [
+    "__title__",
+    "__version__",
+    "__description__",
+    "__credits__",
+    "__readme__",
+    "__requires_python__",
+    "__keywords__",
+    "__status__"
+]
+
+__title__ = "metametameta"
+__version__ = "0.1.4"
+__description__ = "Generate __about__.py with dunder meta."
+__credits__ = "[{'name': 'Matthew Martin', 'email': 'matthewdeanmartin@gmail.com'}]"
+__readme__ = "README.md"
+__requires_python__ = ">=3.8"
+__keywords__ = ['packaging', 'metadata']
+__status__ = "4 - Beta"
