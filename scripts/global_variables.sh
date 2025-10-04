517b1ea6b5737db2aa267ab94f723b3324643154
Matthew Martin <matthewdeanmartin@gmail.com>
2025-10-04 18:04:02 -0400

    0.1.5: precommit on linux

--- scripts/global_variables.sh
+++ scripts/global_variables.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -euo pipefail
 export PIP_DISABLE_PIP_VERSION_CHECK="1"
 export UV_LINK_MODE="copy"
--- scripts/make_source.sh
+++ scripts/make_source.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git2md metametameta \
   --ignore __init__.py __pycache__ \
   --output SOURCE.md
